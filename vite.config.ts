4a65667c201c9af941ccadad4a5d0800a7286a31
Женя Балабанов <biomannumberone@gmail.com>
2024-12-13 21:40:04 +0300

    fix: change config

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,6 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url))
     },
   },
-  base: "./",
+  base: "/AdminPanel/",
   build: { outDir: './docs' },
 });
