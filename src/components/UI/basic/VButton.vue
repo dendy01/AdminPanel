877147d53d5391603bab193c92d8fe989d7ec797
Женя Балабанов <biomannumberone@gmail.com>
2025-01-05 13:49:38 +0300

    fix: types any

--- src/components/UI/basic/VButton.vue
+++ src/components/UI/basic/VButton.vue
@@ -61,9 +61,9 @@ const buttonOutherColor = computed(() => colors[props.btnType]['300']);
 const handleActive = (event: MouseEvent) => {
     if (btn.value) {
         const rect = btn.value.getBoundingClientRect();
-        const size = Math.max(rect.width, rect.height);
-        const x = event.clientX - rect.left - size / 2;
-        const y = event.clientY - rect.top - size / 2;
+        const size: number = Math.max(rect.width, rect.height);
+        const x: number = event.clientX - rect.left - size / 2;
+        const y: number = event.clientY - rect.top - size / 2;
 
         waveStyle.value = {
             top: `${y}px`,
--- src/model/GlobalColors.ts
+++ src/model/GlobalColors.ts
@@ -13,13 +13,13 @@ export enum GlobalColors {
 const tonality: string[] = ['700', '600', '500', '400', '300'];
 
 export const getColors = () => {
-    const color = ref<any>({});
-    const root: any = document.querySelector(':root');
-    const style: any = getComputedStyle(root);
+    const color = ref<Record<string, string>>({});
+    const root: HTMLElement | null = document.querySelector(':root');
+    const style: CSSStyleDeclaration | null = root ? getComputedStyle(root) : null;
 
     Object.values(GlobalColors).forEach((colorItem: string) => {
         color.value[colorItem] = tonality.reduce((acc: any, tonal: string) => {
-            acc[tonal] = style.getPropertyValue(`--color-${colorItem}-${tonal}`);
+            acc[tonal] = style?.getPropertyValue(`--color-${colorItem}-${tonal}`);
             return acc;
         }, {});
     });
